e14991b2e170e9e6de41968174d984bba0af6e01
Sofia <katzmansof@gmail.com>
2023-03-02 19:11:16 -0500

    Finished Lab - Created a Function that takes an array and returns a new array in Title Case

--- index.js
+++ index.js
@@ -11,27 +11,61 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
 
+// THIS FUNCTION TITLE CAPS A STRING
+function characterCaps(str){
+  let newString = str
+  for (let i = 0; i < str.length; i++) { // caps first letter
+    newString = newString.substring(0,1).toUpperCase() + newString.substring(1) 
 
-/* breaking down what the lab asks us to do:
-
-goes through line by line of array and returns the string - using .map()
-goes into returned string and runs function - the function looks for a space, when it finds a space, it turns the index+1 character into uppercase and proceeds to next character
-
-*/
-
-function characterChecker(){
-   for (int i = 0; i < str.length(); i++) {
-      if (str.charAt(i) === " "){
-        str.charAt(i+1).toUpperCase()
-      }
+    if (str.charAt(i) === " "){ // checks for space and caps right after
+      newString = newString.substring(0,i+1) + newString.substring(i+1,i+2).toUpperCase() + newString.substring(i+2)
+      } 
   }
+
+  return newString;    
 }
+
+
+//titleCased(): Maps the Array and Returns a New Array Where Function has Completed A Task 
 
 function titleCased(){
-  
+const titleCased = tutorials.map(characterCaps)
+return titleCased;
 }
 
+
+
+
+
+/*
+FUNCTIONS I WROTE AS PART OF TESTING MAIN FUNCTION THAT I WOULD LIKE TO KEEP FOR FUTURE REFERENCE: 
+
+// TEST: CAPS ONLY FIRST LETTER OF SENTENCE - this works!
+function capsTest(str) {
+  return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+}
+
+// TEST: CAPS SECOND CHARACTER AND RETURNS NEW STRING - this works! 
+function capsSecondCharacter(str) {
+  let newString= str.substring(0,1) + str.substring(1,2).toUpperCase() + str.substring(2).toLowerCase();
+  return newString
+}
+
+// TEST: CAPS THE CHARACTER AFTER A SPACE -- this works only for the first space. 
+  function capsAfterSpace (str){
+  newString = str.toLowerCase() 
+  let i = 0 
+  while (i < str.length+1){
+    if (newString.charAt(i) === " "){
+      newString = newString.substring(0,i+1) + newString.substring(i+1,i+2).toUpperCase() + newString.substring(i+2)
+    return newString
+    }
+    else {
+      i++
+    }
+  }    
+}
+
+
+*/
